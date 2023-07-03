0c9566236fe0735eb6d9fa7b7fc6024df723bcb9
JeonSeongJin <ayclub8866@naver.com>
2023-07-03 11:22:49 +0900

    update: chart.js

--- src/RowThird/RowThird.js
+++ src/RowThird/RowThird.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import MyChart from './MyChart';
 import './RowThird.css'; // RowThird 컴포넌트의 스타일을 정의하는 CSS 파일을 import 합니다.
 
 function RowThird() {
   return (
     <div className="row-third">
     <h2>Charts</h2>
-      {/* 추가적인 JSX 요소를 작성하세요 */}
+    <MyChart/>
     </div>
+
   );
 }
 
